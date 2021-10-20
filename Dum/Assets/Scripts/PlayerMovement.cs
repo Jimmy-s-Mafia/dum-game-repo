778a3b8f33d6c6daad6d86fdde81fc6226c234c9
Daniel Creighton <creightond7@gmail.com>
2021-10-20 17:53:43 -0400

    Added mouse position tracking

--- Dum/Assets/Scripts/PlayerMovement.cs
+++ Dum/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    public Camera cam;
     public float movementSpeed = 5f;
     Vector2 movement;
+    Vector2 mousePosition;
 
     // Start is called before the first frame update
     void Start()
@@ -22,37 +24,18 @@ public class PlayerMovement : MonoBehaviour
         // Movement vertically
         movement.y = Input.GetAxisRaw("Vertical");
 
-
-
-        /*
-        // Amount to move in each dimension
-        float dx = 0;
-        float dy = 0;
-
-        // Move up
-        if (Input.GetKey ("w") || Input.GetKey (KeyCode.UpArrow)) {
-            dy = distancePerSecond * Time.deltaTime;
-        }
-        // Move down
-        if (Input.GetKey ("s") || Input.GetKey (KeyCode.DownArrow)) {
-            dy = -distancePerSecond * Time.deltaTime;
-        }
-        // Move left
-        if (Input.GetKey ("a") || Input.GetKey (KeyCode.LeftArrow)) {
-            dx = -distancePerSecond * Time.deltaTime;
-        }
-        // Move right
-        if (Input.GetKey ("d") || Input.GetKey (KeyCode.RightArrow)) {
-            dx = distancePerSecond * Time.deltaTime;
-        }
-        // Move by that amount
-        rb.position += new Vector2(dx, dy);
-        */
+        // Find current mouse position
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate() 
     {
         // Movement
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+
+        // Direction for player to look
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
     }
 }
