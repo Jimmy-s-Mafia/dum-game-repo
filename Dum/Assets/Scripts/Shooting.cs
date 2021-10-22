6f5f3d24f0ff7686c7020d3669b85739f0316348
Daniel Creighton <creightond7@gmail.com>
2021-10-22 14:18:42 -0400

    Added shooting, made changes to sprite sizes

---
+++ Dum/Assets/Scripts/Shooting.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Create bullet
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        // Get bullet rigidbody
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        // Fire bullet
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
