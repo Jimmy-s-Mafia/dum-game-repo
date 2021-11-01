b5aade85eed76864bd049a45d0ba2502d061135b
Kelly Lam <klam@student.ysu.edu>
2021-10-31 21:43:11 -0400

    Added pause menu

---
+++ Dum/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public static bool GameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when esc key is pressed, toggle pause/resume
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+}
